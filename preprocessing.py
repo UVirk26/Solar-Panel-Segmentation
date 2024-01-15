a78aff41bca43bcb69949cde5ceb85cce99497f5
UVirk26 <100397869+UVirk26@users.noreply.github.com>
2024-01-15 10:13:29 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -8,3 +8,97 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import os
 from PIL import Image
+from utils import *
+
+#copy all images to image folder and save labels to label folder with same name as correspoding image
+
+root_dir = ''
+resolution = ''
+data_dir = os.path.join(root_dir)#,resolution)
+
+image_root = '/kaggle/working/train'
+label_root = '/kaggle/working/train_masks'
+if not os.path.isdir(image_root):
+    os.mkdir(image_root)
+if not os.path.isdir(label_root):
+    os.mkdir(label_root)
+
+images = list()
+labels = list()
+
+for (dirpath, dirnames, filenames) in os.walk(data_dir):
+    # img_names += [os.path.join(dirpath, file) for file in filenames]
+    images += [os.path.join(dirpath, file) for file in filenames]
+
+labels += [i for i in filter(lambda score: '_label.bmp' in score, images)]
+images = [i for i in filter(lambda score: '_label.bmp' not in score, images)]
+
+for img_path in images:
+    src_path = img_path
+    dst_path = os.path.join(image_root,os.path.basename(img_path))
+    img = Image.open(src_path)
+    new_img = img.resize( (256, 256) )
+    new_img.save( dst_path[:-4]+'.png', 'png')
+
+for label_path in labels:
+    src_path = label_path
+    file_name = os.path.basename(label_path).replace('_label','')
+    dst_path = os.path.join(label_root,file_name)
+    img = Image.open(src_path)
+    new_img = img.resize( (256, 256) )
+
+print("Train set:  ", len(os.listdir("/kaggle/working/train")))
+print("Train masks:", len(os.listdir("/kaggle/working/train_masks")))
+
+df = prepare_dataframe('/kaggle/working/train', "solar_path")
+mask_df = prepare_dataframe('/kaggle/working/train_masks', "mask_path")
+df["mask_path"] = mask_df["mask_path"]
+
+img_size = [256,256]
+
+def data_augmentation(solar_img, mask_img):
+
+    if tf.random.uniform(()) > 0.5:
+        solar_img = tf.image.flip_left_right(solar_img)
+        mask_img = tf.image.flip_left_right(mask_img)
+
+    return solar_img, mask_img
+
+def preprocessing(solar_path, mask_path):
+    solar_img = tf.io.read_file(solar_path) 
+    solar_img = tf.image.decode_jpeg(solar_img, channels=3)
+    solar_img = tf.image.resize(solar_img, img_size)
+    solar_img = tf.cast(solar_img, tf.float32) / 255.0
+    
+    mask_img = tf.io.read_file(mask_path)
+    mask_img = tf.image.decode_jpeg(mask_img, channels=3)
+    mask_img = tf.image.resize(mask_img, img_size)
+    mask_img = mask_img[:,:,:1]    
+    mask_img = tf.math.sign(mask_img)
+    
+    
+    return solar_img, mask_img
+
+def create_dataset(df, train = False):
+    if not train:
+        ds = tf.data.Dataset.from_tensor_slices((df["solar_path"].values, df["mask_path"].values))
+        ds = ds.map(preprocessing, tf.data.AUTOTUNE)
+    else:
+        ds = tf.data.Dataset.from_tensor_slices((df["solar_path"].values, df["mask_path"].values))
+        ds = ds.map(preprocessing, tf.data.AUTOTUNE)
+        ds = ds.map(data_augmentation, tf.data.AUTOTUNE)
+
+    return ds
+
+train_df, valid_df = train_test_split(df, random_state=42, test_size=.25)
+train = create_dataset(train_df, train = True)
+valid = create_dataset(valid_df)
+
+TRAIN_LENGTH = len(train_df)
+BATCH_SIZE = 24
+BUFFER_SIZE = 1000
+
+train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+valid_dataset = valid.batch(BATCH_SIZE)
+
