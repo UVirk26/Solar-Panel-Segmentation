2d0b62868c8c4e90198af268129dc091202e6834
UVirk26 <100397869+UVirk26@users.noreply.github.com>
2024-01-15 10:01:43 +0530

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import pandas as pd 
+import shutil
+import tensorflow as tf
+from zipfile import ZipFile
+import keras.backend as K
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+from utils import *
+from model import *
+from train import *
+
+
+def visualize(display_list):
+    plt.figure(figsize=(15, 15))
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i+1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
+
+def show_predictions(sample_image, sample_mask):
+    pred_mask = model.predict(sample_image[tf.newaxis, ...])
+    pred_mask = pred_mask.reshape(img_size[0],img_size[1],1)
+    visualize([sample_image, sample_mask, pred_mask])
+    
+show_predictions(sample_image, sample_mask)
---
+++ preprocessing.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd 
+import shutil
+import tensorflow as tf
+from zipfile import ZipFile
+import keras.backend as K
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
